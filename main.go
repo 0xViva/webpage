f1661b1edbb0a5877de5abffb8fe7982e10b496d
0xViva <0xViva@proton.me>
2025-03-14 13:47:03 +0100

    dynamic webpage titles based on domain

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/0xViva/webpage/views"
 	"github.com/a-h/templ"
 	"github.com/labstack/echo"
@@ -21,10 +23,27 @@ func main() {
 }
 
 func homeHandler(c echo.Context) error {
+	host := c.Request().Host
+	title := getTitleFromDomain(host)
 
-	return render(c, views.Home("0xViva's Webpage"))
+	return render(c, views.Home(title))
 }
 
 func render(ctx echo.Context, cmp templ.Component) error {
 	return cmp.Render(ctx.Request().Context(), ctx.Response())
+}
+func getTitleFromDomain(domain string) string {
+
+	mapping := map[string]string{
+		"augustg.dev": "August's webpage",
+		"0xviva.dev":  "0xViva's webpage",
+		"localhost":   "Localhost",
+	}
+
+	host := strings.Split(domain, ":")[0]
+
+	if title, exists := mapping[host]; exists {
+		return title
+	}
+	return "Default Title"
 }
